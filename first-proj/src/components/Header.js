ed822c85c113faf6f3e146b8312ded27f471e4eb
Rubik <rubik.flush@gmail.com>
2020-12-08 16:23:49 +0300

    learn #12

---
+++ first-proj/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ first-proj/src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <div className="sidebar">
+            <nav>
+                <ul>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="#">Messages</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Music</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
